2033300f6e55453ada688a593c4aec7d7dc7a2fb
Stanislav Reva <RevaSA95@gmail.com>
2020-03-19 23:15:28 +0300

    Refactoring

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,31 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
-};
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/airbnb',
+    ],
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    rules: {
+        indent: ['error', 4, { SwitchCase: 1 }],
+        'no-unused-vars': ['warn'],
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    },
+    overrides: [
+        {
+            files: ['*.vue'],
+            rules: {
+                indent: 'off',
+                'vue/script-indent': ['error', 4, {
+                    baseIndent: 1,
+                    switchCase: 1,
+                }],
+            },
+        },
+    ],
+}
