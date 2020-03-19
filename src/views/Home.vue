46b460deff2b99a21ac3047011c5b70ed7f660b7
Stanislav Reva <RevaSA95@gmail.com>
2020-03-19 23:51:20 +0300

    Add vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,5 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <h1>Valar Morgulis</h1>
     </div>
 </template>
-
-<script>
-    import HelloWorld from '@/components/HelloWorld.vue'
-
-    export default {
-        name: 'Home',
-        components: {
-            HelloWorld,
-        },
-    }
-</script>
