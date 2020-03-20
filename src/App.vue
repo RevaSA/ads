c79708233fea89e02da91495a936d99be715827e
Stanislav Reva <RevaSA95@gmail.com>
2020-03-20 14:03:11 +0300

    Add nav links to drawer and toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
     <v-app>
-        <v-navigation-drawer app temporary/>
+        <v-navigation-drawer app temporary v-model="drawer">
+            <v-list>
+                <v-list-item v-for="link in navLinks" :key="link.title" :to="link.url">
+                    <v-list-item-icon>
+                        <v-icon>{{ link.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title v-text="link.title"/>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
 
         <v-app-bar app dark>
-            <v-app-bar-nav-icon/>
+            <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"/>
             <v-toolbar-title>Ad application</v-toolbar-title>
             <v-spacer/>
-            <v-toolbar-items>
-                <v-btn text>
-                    <v-icon left>mdi-link</v-icon>
-                    Link
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn text v-for="link in navLinks" :key="link.title" :to="link.url">
+                    <v-icon left>{{ link.icon }}</v-icon> {{ link.title }}
                 </v-btn>
             </v-toolbar-items>
         </v-app-bar>
@@ -19,3 +30,38 @@
         </v-content>
     </v-app>
 </template>
+
+<script>
+    export default {
+        data: () => ({
+            drawer: false,
+            navLinks: [
+                {
+                    title: 'Login',
+                    icon: 'mdi-lock',
+                    url: '/login',
+                },
+                {
+                    title: 'Registration',
+                    icon: 'mdi-face',
+                    url: '/registration',
+                },
+                {
+                    title: 'Orders',
+                    icon: 'mdi-bookmark',
+                    url: '/orders',
+                },
+                {
+                    title: 'New ad',
+                    icon: 'mdi-note',
+                    url: '/new',
+                },
+                {
+                    title: 'My ads',
+                    icon: 'mdi-format-list-bulleted',
+                    url: '/list',
+                },
+            ],
+        }),
+    }
+</script>
