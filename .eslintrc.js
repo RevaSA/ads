924b3592970716582612b19fc72c6f35f5220a59
Stanislav Reva <RevaSA95@gmail.com>
2020-03-20 16:15:45 +0300

    Cnange eslint arrow-parens

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'import/extensions': 'off',
+        'arrow-parens': ['error', 'as-needed'],
     },
     overrides: [
         {
