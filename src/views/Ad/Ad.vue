36cfdaa574d4e4232b1ffbf1abf5d137f0962c29
Stanislav Reva <RevaSA95@gmail.com>
2020-03-23 00:13:22 +0300

    Make ad page

--- src/views/Ad/Ad.vue
+++ src/views/Ad/Ad.vue
@@ -1,10 +1,23 @@
 <template>
     <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Ad</h1>
-            </v-flex>
-        </v-layout>
+        <v-row>
+            <v-col>
+                <v-card>
+                    <v-img height="300" src="https://cdn.vuetifyjs.com/images/carousel/planet.jpg"/>
+
+                    <v-card-text>
+                        <h1 class="text--primary">lorem</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, ipsa?</p>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn>Edit</v-btn>
+                        <v-btn class="primary">Buy</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
