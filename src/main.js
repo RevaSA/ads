03d9a7e734a4c3db927b6d28b3c8325c82709e41
Stanislav Reva <RevaSA95@gmail.com>
2020-03-23 10:16:22 +0300

    Make store for user and add firebase

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import * as firebase from 'firebase';
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -11,4 +12,15 @@ new Vue({
     store,
     vuetify,
     render: h => h(App),
+    created() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyDkYEEOmmTnjXDDOqryqzf3oYF3yoEa4RM',
+            authDomain: 'ads-project-7d478.firebaseapp.com',
+            databaseURL: 'https://ads-project-7d478.firebaseio.com',
+            projectId: 'ads-project-7d478',
+            storageBucket: 'ads-project-7d478.appspot.com',
+            messagingSenderId: '286586016164',
+            appId: '1:286586016164:web:70ad9ae16a3d97733db131',
+        })
+    },
 }).$mount('#app')
