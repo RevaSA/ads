9bdf00e47d7126b9ae9e84de3bae90f964bfeb14
Stanislav Reva <RevaSA95@gmail.com>
2020-03-28 18:35:15 +0300

    Add travis

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
+const publicPath = process.env.NODE_ENV === 'production' ? '/ads/' : '/'
+
+console.log(publicPath);
+
 module.exports = {
+    publicPath,
     transpileDependencies: [
         'vuetify',
     ],
